8507ceff7793e3d3d31e956aa650627660b2304f
filip <filda.kuchar@seznam.cz>
2020-11-01 13:22:43 +0100

    Fix up psr 18 client exception (use custom exception, not from guzzle)

--- src/Client/ClientException.php
+++ src/Client/ClientException.php
@@ -4,9 +4,38 @@ declare(strict_types=1);
 
 namespace Mistrfilda\Pid\Api\Client;
 
-use GuzzleHttp\Exception\ClientException as GuzzleException;
+use Exception;
 use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
 
-class ClientException extends GuzzleException implements ClientExceptionInterface
+class ClientException extends Exception implements ClientExceptionInterface
 {
+	/** @var RequestInterface */
+	private $request;
+
+	/** @var ResponseInterface|null */
+	private $response;
+
+	public function __construct(
+		string $message,
+		RequestInterface $request,
+		?ResponseInterface $response,
+		?Throwable $previous
+	) {
+		parent::__construct($message, 0, $previous);
+		$this->request = $request;
+		$this->response = $response;
+	}
+
+	public function getRequest(): RequestInterface
+	{
+		return $this->request;
+	}
+
+	public function getResponse(): ?ResponseInterface
+	{
+		return $this->response;
+	}
 }
